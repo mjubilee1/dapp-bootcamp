bcd5a3f66be310567b247c2cc29298b4d34b8d4e
Montrell <jubileeinvestmentsdmv@gmail.com>
2022-03-27 15:30:24 -0400

    finish exchange code

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -3,25 +3,61 @@ pragma solidity 0.8.13;
 
 import './Token.sol';
 
-// TODO:
-// [X] Set the fee account
-// [X] Deposit Ether
-// [X] Withdraw Ether
-// [X] Deposit tokens
-// [] Withdraw tokens
-// [X] Check balances
-// [] Make order
-// [] Cancel order
-// [] Fill order
-
 contract Exchange {
 	address public feeAccount; // the account that receives exchange fees
 	address constant ETHER = address(0); // store Ether in tokens mapping with blank address
 	uint256 public feePercent; // the fee percentage
 	mapping(address => mapping(address => uint256)) public tokens;
+	mapping(uint256 => _Order) public orders;
+	mapping(uint256 => bool) public orderCancelled;
+	mapping(uint256 => bool) public orderFilled;
+	uint256 public orderCount;
 
+	// Events
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
 	event Withdraw(address token, address user, uint256 amount, uint256 balance);
+	event Order(
+		uint id,
+		address user,
+		address tokenGet,
+		uint amountGet,
+		address tokenGive,
+		uint amountGive,
+		uint timestamp
+	);
+	event CancelOrder(
+		uint256 id,
+		address user,
+		address tokenGet,
+		uint256 amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		uint256 timestamp
+	);
+	event Trade(
+		uint256 id,
+		address user,
+		address tokenGet,
+		uint256 amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		address userFill,
+		uint256 timestamp
+	);
+
+	struct _Order {
+		uint id;
+		address user;
+		address tokenGet;
+		uint amountGet;
+		address tokenGive;
+		uint amountGive;
+		uint timestamp;
+	}
+
+	// a way to model the order
+	// a way store the order
+	// add order to storage
 
 	constructor(address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
@@ -62,5 +98,59 @@ contract Exchange {
 
 	function balanceOf(address _token, address _user) public view returns (uint256) {
 		return tokens[_token][_user];
+	}
+
+	function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+		orderCount = orderCount + 1;
+		orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+		emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+	}
+
+	function cancelOrder(uint256 _id) public {
+		_Order storage _order = orders[_id];
+		require(address(_order.user) == msg.sender);
+		require(_order.id == _id);
+
+		orderCancelled[_id] = true;
+		emit CancelOrder(
+			_order.id,
+			msg.sender,
+			_order.tokenGet,
+			_order.amountGet,
+			_order.tokenGive,
+			_order.amountGive,
+			block.timestamp
+		);
+	}
+
+	function fillOrder(uint256 _id) public {
+		require(_id > 0 && _id <= orderCount);
+		require(!orderFilled[_id]);
+		require(!orderCancelled[_id]);
+		_Order storage _order = orders[_id];
+		_trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+		orderFilled[_order.id] = true;
+	}
+
+	function _trade(
+		uint256 _orderId,
+		address _user,
+		address _tokenGet,
+		uint256 _amountGet,
+		address _tokenGive,
+		uint256 _amountGive
+		) internal {
+		// Fee paid by the user that fills the order, a.k.a msg.sender
+		// Fee deducted from _amountGet
+		uint256 _feeAmount = (_amountGive * feePercent) / 100;
+
+		// Execute trade
+		tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender] - (_amountGet + _feeAmount);
+		tokens[_tokenGet][_user] = tokens[_tokenGet][_user] + _amountGet;
+		tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount] + _feeAmount;
+		tokens[_tokenGive][_user] = tokens[_tokenGive][_user] - _amountGive;
+		tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender] + _amountGet;
+
+		emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, block.timestamp);
 	}
 }
